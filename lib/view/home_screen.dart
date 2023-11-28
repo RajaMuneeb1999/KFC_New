6a905a2e75525469891860b8934d3f6ee97e0754
irfan <shahhasanhussain@gmail.com>
2023-11-28 16:32:04 +0500

    KFC

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -226,7 +226,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       backgroundImage: MemoryImage(bytes),
                     ),
                     accountName: Text(
-                      'Welcome '
+                      'Welcome Muneeb'
                       '$firstName_',
                       style: const TextStyle(
                           fontSize: 16.0, fontFamily: "Brand Bold"),
